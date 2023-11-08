54e21604b38330498378a2042f4db2cfd6a53f23
simonchan66 <formul529@gmail.com>
2023-11-08 11:23:52 -0700

    first commit

---
+++ Event.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab6_serialization
+{
+    internal class Event
+    {
+        public int EventNumber {  get; set; }
+        public string Location { get; set; }
+        public string EventName { get; set; }
+        //default constructor because there is no code inside of this
+        public Event() { 
+        
+        }
+        public Event(int eventNumber, string location, string eventName)
+        {
+            this.EventNumber = eventNumber;
+            this.Location = location;
+            this.EventName = eventName;
+        }
+
+        public void SerializeDeserializeStream() {
+            string path = "name.bin";
+            StreamWriter streamWriter = File.CreateText(path);
+            streamWriter.WriteLine("Hackathon");
+            streamWriter.Close();
+
+            using (StreamReader reader = File.OpenText(path))
+            {
+                string word = reader.ReadToEnd().TrimEnd();
+                char first = word[0];
+                
+                char middle = word[(int)(word.Length /2)];
+                char last = word[word.Length - 1];
+                Console.WriteLine("In Word: Hackathon");
+                Console.WriteLine("The First Character is: " + first);
+                Console.WriteLine("The Middle Character is: " + middle);
+                Console.WriteLine("The Last Character is: " + last);
+
+
+
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+
+namespace lab6_serialization
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Event e = new Event(1, "Calgary", "Tech Competition");
+            //Console.WriteLine(e.EventNumber);
+            //Console.WriteLine(e.Location);
+            string encoded = JsonSerializer.Serialize(e);
+            Console.WriteLine(encoded);
+
+            string path = "events.json";
+
+            File.WriteAllText(path, encoded);
+            Console.WriteLine("successfully saved to JSON file");
+
+
+            // deserialize
+            string contents = File.ReadAllText(path);
+            //JsonSerializer.Deserialize(encoded, typeof(Event));
+            Event decoded = JsonSerializer.Deserialize<Event>(contents);
+            Console.WriteLine(decoded.EventNumber);
+            Console.WriteLine(decoded.Location);
+            Console.WriteLine(decoded.EventName);
+
+            //Readfromfile
+
+            e.SerializeDeserializeStream();
+        }
+    }
+}
